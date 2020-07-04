4aa14a8e056ccb852a61fa9e4348bfccd79756a9
lizhanglin <2666185@qq.com>
2020-07-04 20:59:33 +0100

    added a few appraoches for dealing with single and double words query

--- main.cpp
+++ main.cpp
@@ -22,11 +22,32 @@ using namespace std;
 // - enable relevance ordering: e.g. if we search for
 // - replace text document with json document, and enabling searching by json field
 
+// word -> document mapping
+// China -> [ (0, [0, 3]), (1, [0, 5]), (2, [0]) ]
+// Eastern -> [ (0, [2, 4]), (1, [4]) ]
+// Easter -> []
+
+// other word -> [documentId]
+
+// time complexity (n*m) - n number of documents in the DB, average m number of characters in the doducment
+// space complexity (m)
+// brute force
+
+// constraints:
+// - strong match X
+// - weak match Y
+
+// 1. single word -> China:
+// 2. double words -> China Eastern:
+// 3. missing pending character -> China Easter: -> Easter && Eastern && East
+// 4. typo of single character AND typo doesn't match any existing words in the index ->
+
+
 vector<string> documents = {
-        "I'm the first log",
-        "Here is the second log",
-        "How are you doing?",
-        "Hello Google"
+        "China ashjdas Eastern China Eastern airline offers airliairline offers airline tickets & flights to over worldwide 1000 destinations in 177 countries. Book direct at China Eastern UK official site.",
+        "China offers asdhsajdhj as Eastern China airline tickets & flights to over worldwide 1000 destinations in 177 countries. Book direct at China Eastern UK official site.",
+        "China Eastern offers airliairline offers airline tickets & flights to over worldwide 1000 destinations in 177 countries. Book direct at China Eastern UK official site.",
+        "China Southern Airlines UK, London, United Kingdom. 10K likes. Welcome to the official page of China Southern Airlines in United Kingdom"
 };
 
 unordered_map<string, vector<int>> termsToDocumentIdsMap = {};
